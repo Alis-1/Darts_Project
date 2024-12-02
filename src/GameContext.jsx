6cd29e7e23b1e78c224436baa121255e42b45a35
Alis-1 <tatu.tammi@edu.turkuamk.fi>
2024-12-02 17:58:40 +0200

    GameContext: Set up GameContext for managing game state

--- src/GameContext.jsx
+++ src/GameContext.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable react/prop-types */
 import { createContext, useState } from "react";
 
 // Luo konteksti
 export const GameContext = createContext();
-
 
 // Luo Provider-komponentti
 export const GameProvider = ({ children }) => {
@@ -19,4 +17,3 @@ export const GameProvider = ({ children }) => {
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
 };
-
