dce3f3e4a7b5aa9b05ae59a394ffdcf4f1a30cd6
VilleMäkinen <ville.makinen03@gmail.com>
2024-12-12 18:28:32 +0200

    setsWon lisätty ja fixejä

--- src/components/Scoreboard.jsx
+++ src/components/Scoreboard.jsx
@@ -2,29 +2,52 @@ import { useContext, useState } from 'react';
 import { GameContext } from "../GameContext";
 
 const Scoreboard = () => {
-  const { players, setPlayers } = useContext(GameContext);
+  const { gameMode,players, setPlayers } = useContext(GameContext);
   const [inputValue, setInputValue] = useState('');
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
 
   const handleAddScore = () => {
     if (inputValue.trim() !== '' && players[currentPlayerIndex]) {
       const newScore = parseInt(inputValue);
+      const validTurn = true;
+
+      const resetScores = () => {
+        const resetScore = gameMode === "301" ? 301 : 501;
+        return players.map(player => ({ ...player, score: resetScore }));
+      };
+
       const updatedPlayers = players.map((player, index) => {
         if (index === currentPlayerIndex) {
+
           if ((player.score - newScore) < 0) {
             alert("Invalid score, results in negative");
+            setInputValue('');
+            validTurn = false;
+            return player;
           }
+
           if (player.score - newScore === 0) {
             alert(player.name + " wins!");
-          } else {
+            setInputValue('');
+            player.setsWon++;
+            const resetPlayers = resetScores(); //pisteiden resetointi voiton jälkeen
+            setPlayers(resetPlayers);
+            setCurrentPlayerIndex(0);
+            validTurn = false; //jotta vuoro ei vaihdu seuraavaan pelaajaan
+            return { ...player, setsWon: player.setsWon }; // Update winning player
+          } 
+          else {
             return { ...player, score: player.score - newScore };
           }
         }
         return player;
       });
+      //vaihda seuraavaan pelaajaan vain jos heitto oli valid
+      if (validTurn) {
       setPlayers(updatedPlayers);
-      setInputValue('');
       setCurrentPlayerIndex((currentPlayerIndex + 1) % players.length);
+    }
+      setInputValue('');
     }
   };
 
@@ -50,7 +73,7 @@ const Scoreboard = () => {
       <ul>
         {players.map((player, index) => (
           <li key={index}>
-            {player.name}: {player.score} points
+            {player.name}: {player.score} points {player.setsWon} rounds won
           </li>
         ))}
       </ul>
